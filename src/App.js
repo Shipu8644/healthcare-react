0ca9c9ac9a897447950a13a0751db20f9c265c5c
Shipu8644 <shipu.monjurur@gmail.com>
2021-10-18 02:44:19 +0600

    header done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from '../src/pages/Shared/Header/Header'
+import Home from './pages/Home/Home/Home';
+import NotFound from './pages/NotFound/NotFound';
+
 
 function App() {
 
   return (
     <div className="App">
+      <BrowserRouter>
+        <Header></Header>
+        <Switch>
+          <Route exact path='/'>
+            <Home></Home>
+          </Route>
+          <Route path='/home'>
+            <Home></Home>
+          </Route>
 
+          <Route to='*'>
+            <NotFound></NotFound>
+          </Route>
+        </Switch>
+      </BrowserRouter>
 
     </div>
   );
---
+++ src/pages/Shared/Header/Header.js
@@ -0,0 +1,51 @@
+
+import React from 'react';
+import { Container, Form, FormControl, Nav, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <div>
+            <Navbar className='text-white' bg="info" expand="lg" >
+                <Container fluid className="container " >
+                    <Navbar.Brand className="text-white me-5" href="#">Baby Care</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="navbarScroll" />
+                    <Navbar.Collapse id="navbarScroll"  >
+                        <Nav
+                            className="me-auto my-2 my-lg-0"
+                            style={{ maxHeight: '150px' }}
+                            navbarScroll
+                        >
+                            <Nav.Link as={Link} className="text-white me-3" to='/home'>Home</Nav.Link>
+                            <Nav.Link className="text-white me-3" href="#action2">About Us</Nav.Link>
+                            <Nav.Link className="text-white me-3" href="#action2">Services</Nav.Link>
+                            <Nav.Link className="text-white me-3" href="#action2">Doctors</Nav.Link>
+                            <Nav.Link className="text-white me-3" href="#action2">Blog</Nav.Link>
+                            <Nav.Link className="text-white me-3" href="#action2">Contact Us</Nav.Link>
+
+                        </Nav >
+
+
+                        <Nav.Link className="text-white me-2" href="#action2">Login</Nav.Link>
+                        <span className="text-warning me-2">Sign in as: Monjurur Kader Shipu</span>
+
+                        <Form className="d-flex">
+
+                            <FormControl
+                                type="search"
+                                placeholder="Search"
+                                className="me-2"
+                                aria-label="Search"
+                            />
+
+
+                        </Form>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+
+        </div>
+    );
+};
+
+export default Header;
